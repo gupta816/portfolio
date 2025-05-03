9f9a4b8a6e165e2cbc983863630265187fa068e1
gupta816 <kumar.tushar816@gmail.com>
2025-05-03 16:43:18 +0530

    flex

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     <nav className="fixed top-0 left-0 w-full bg-white shadow z-50">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
         <span className="font-bold text-lg">Tushar Kumar</span>
-        <div className="space-x-6">
+        <div className="space-x-6 flex flex-wrap">
           <Link href="/">Home</Link>
           <Link href="/#aboutme">About Me</Link>
           <Link href="/#projects">Projects</Link>
